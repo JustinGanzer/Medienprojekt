89326efd34f4e723801da02bc8afd9eeadff57a2
Vincent Helmreich <vincent@helmreich.me>
2016-04-06 10:52:00 +0200

    php-dateien gelöscht

--- php/sethighscore.php
+++
@@ -1,7 +0,0 @@
-<?php
-	mysql_connect("localhost","root","");
-	mysql_select_db("highscore");
-	mysql_query("SET CHARACTER SET utf8");
-	mysql_query("INSERT INTO highscore (name, points) VALUES ('{$_POST['name']}' , '{$_POST['points']}')");
-	
-?>
